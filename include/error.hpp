0d533a35ffaf160ad079c9118cf3c3ab3cc5ec91
CppPhil <CppPhil@users.noreply.github.com>
2020-07-26 21:53:24 +0200

    Basic Error type

---
+++ include/error.hpp
@@ -0,0 +1,59 @@
+#ifndef INCG_BFC_ERROR_HPP
+#define INCG_BFC_ERROR_HPP
+#include <cstddef>
+
+#include <iosfwd>
+#include <string>
+
+#include <tl/expected.hpp>
+
+#include <pl/current_function.hpp>
+
+#define BFC_ERROR_KIND BFC_ERROR_KIND_X(InvalidArgument)
+
+namespace bfc {
+class [[nodiscard]] Error
+{
+public:
+  enum Kind {
+#define BFC_ERROR_KIND_X(kind) kind,
+    BFC_ERROR_KIND
+#undef BFC_ERROR_KIND_X
+  };
+
+  friend std::ostream& operator<<(std::ostream& os, const Error& error);
+
+  Error(
+    Kind        kind,
+    std::string file,
+    std::string function,
+    std::size_t line,
+    std::string message);
+
+  Kind kind() const noexcept;
+
+  const std::string& file() const noexcept;
+
+  const std::string& function() const noexcept;
+
+  std::size_t line() const noexcept;
+
+  const std::string& message() const noexcept;
+
+private:
+  Kind        m_kind;
+  std::string m_file;
+  std::string m_function;
+  std::size_t m_line;
+  std::string m_message;
+};
+
+template<typename Ty>
+using Expected = tl::expected<Ty, Error>;
+} // namespace bfc
+
+#define BFC_UNEXPECTED(kind, message) \
+  ::tl::make_unexpected( \ 
+    ::bfc::Error{kind, __FILE__, PL_CURRENT_FUNCTION, __LINE__, message})
+
+#endif // INCG_BFC_ERROR_HPP
---
+++ src/error.cpp
@@ -0,0 +1,60 @@
+#include <ostream>
+#include <utility>
+
+#include <fmt/format.h>
+#include <fmt/ostream.h>
+
+#include <pl/string_view.hpp>
+
+#include "error.hpp"
+
+namespace bfc {
+namespace {
+inline pl::string_view kind_to_string(Error::Kind kind)
+{
+  switch (kind) {
+#define BFC_ERROR_KIND_X(kind) \
+  case Error::kind: return #kind;
+    BFC_ERROR_KIND
+#undef BFC_ERROR_KIND_X
+  }
+
+  // TODO: UNREACHABLE
+}
+} // namespace
+
+std::ostream& operator<<(std::ostream& os, const Error& error)
+{
+  return os << fmt::format(
+           R"(Error{{"kind": "{}", "file": "{}", "function": "{}", "line"    : {}, "message": "{}"}})",
+           kind_to_string(error.kind()),
+           error.file(),
+           error.function(),
+           error.line(),
+           error.message());
+}
+
+Error::Error(
+  Kind        kind,
+  std::string file,
+  std::string function,
+  std::size_t line,
+  std::string message)
+  : m_kind{kind}
+  , m_file{std::move(file)}
+  , m_function{std::move(function)}
+  , m_line{line}
+  , m_message{std::move(message)}
+{
+}
+
+Error::Kind Error::kind() const noexcept { return m_kind; }
+
+const std::string& Error::file() const noexcept { return m_file; }
+
+const std::string& Error::function() const noexcept { return m_function; }
+
+std::size_t Error::line() const noexcept { return m_line; }
+
+const std::string& Error::message() const noexcept { return m_message; }
+} // namespace bfc
